c84a55e6685581987a26f8b32f3ba5832a7c1d25
KalaivaniKTL <107670204+KalaivaniKTL@users.noreply.github.com>
2022-07-17 20:36:33 +0530

    Add files via upload

---
+++ webapp-jenkins/src/main/java/com/cts/webappjenkins/WebappJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.cts.webappjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappJenkinsApplication.class, args);
+	}
+
+}
